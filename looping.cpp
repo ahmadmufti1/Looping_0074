540d715403d87e7948640d664528319535bc5813
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-31 20:30:59 +0800

    membuat fungsi perulangan dengan for, mencetak tulisan sebanyak 5 kali dan mengahasilkan bilangan acak

--- looping.cpp
+++ looping.cpp
@@ -2,37 +2,17 @@
 #include <ctime>
 using namespace std;
 
+
 int main(){
     int x;
-
+// Perulangan dengan for
+// Mencetak tulisan sebanyak 5 kali
     cout<<"Perulangan for"<<endl;
     for(int i=0; i<=4; i++){
         cout<<"Teknologi Informasi"<<endl;
     }
     cout<<endl;
-
+// untuk menghasilkan bilangan acak
     srand(time(0));
 
-    cout<<"Perulangan while"<<endl;
-    x = 1 + rand() % 10;
-
-    while(x <= 5){
-        cout<<"Bilangan acak =  "<<x <<endl;
-        x = rand() % 10;
-
-    }
-
-    cout<< "Bilangan acak while yang terakhir = "<<x <<endl;
-    cout<<endl;
-
-    //Perulangan dengan do while
-    cout<<"Perulangan do while"<<endl;
-    x = 1 + rand()%10;
-    do{
-        cout<<"Bilangan acak = "<<x <<endl;
-        x = rand() % 10;
-    }while(x <= 5);
-
-    cout<<"Bilangan acak do while yang terakhir = "<<x <<endl;
-    cout<<endl;
 }
