04d4a5e4c99c5850160124fe2731356188df0654
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-31 20:08:16 +0800

    membuat fungsi input untuk nilai matematika dan bahasa inggris

--- logika_and.cpp
+++ logika_and.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main(){
     float nilB, nilM, rerata;
     string status;
+// Input nilai Matematika dan Bahasa Inggris
+    cout << "Masukkan nilai Matematika: ";
+    cin >> nilM;
 
+    cout << "Masukkan nilai Bahasa Inggris: ";
+    cin >> nilB;
+
+    
 }
