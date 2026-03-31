8c12de526cc38eee13d4dcfac85cb8b7530e2b77
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-31 20:23:07 +0800

    membuat deklarasi variabel global

--- logika_or.cpp
+++ logika_or.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//deklarasi variabel global
 int main(){
     float nilB, nilM, rerata;
     string status;
-
-    cout << "Masukkan nilai Matematika: ";
-    cin >> nilM;
-
-    cout << "Masukkan nilai Bahasa Inggris: ";
-    cin >> nilB;
-
-    rerata = (nilM + nilB) / 2;
-
-    if(( rerata >= 60) || (rerata >= 70))
-    {
-        status = "Lulus";
-    }else{
-        status = "Tidak Lulus";
-    }
-
-    cout << "Status kelulusan : " << status<< ", dengan nilai rata-rata  : " << rerata << endl;
 }
