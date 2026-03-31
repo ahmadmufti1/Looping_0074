cb7c63d68102a74760177ad299cfe1523761a96b
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-31 20:24:17 +0800

    membuat fungsi untuk menginput nilai matematika dan bahasa inggris

--- logika_or.cpp
+++ logika_or.cpp
@@ -5,4 +5,11 @@ using namespace std;
 int main(){
     float nilB, nilM, rerata;
     string status;
+    
+// Input nilai Matematika dan Bahasa Inggris
+    cout << "Masukkan nilai Matematika: ";
+    cin >> nilM;
+
+    cout << "Masukkan nilai Bahasa Inggris: ";
+    cin >> nilB;
 }
